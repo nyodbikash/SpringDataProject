7598bf3e597b127a927714ba4699b2f45cf53483
Bikash Nayak <nbikash199@gmail.com>
2021-07-31 11:17:07 +0530

    some files added

--- FirstActuatorProject/src/main/java/com/bikash/controller/ActuatorController.java
+++ FirstActuatorProject/src/main/java/com/bikash/controller/ActuatorController.java
@@ -1,5 +1,13 @@
 package com.bikash.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ActuatorController {
+	@GetMapping("/hello")
+	public String helloUser() {
+		return "hello User";
+	}
 
 }
